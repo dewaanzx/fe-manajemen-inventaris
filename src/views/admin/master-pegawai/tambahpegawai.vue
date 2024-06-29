1420eac308e062f715dd3eb4c5e93259e81d3c32
dewaanzx <dewabandis11@student.uns.ac.id>
2024-06-29 20:11:31 +0700

    last

--- src/views/admin/master-pegawai/tambahpegawai.vue
+++ src/views/admin/master-pegawai/tambahpegawai.vue
@@ -6,7 +6,7 @@
 	      <div class="">
 	        <div class="grid md:flex md:flex-row md:justify-around gap-2">
 	          <span class="text-[14px] md:text-[16px] md:w-[20%] text-[#2B9FDC]"
-	            >Informasi Ruangan</span
+	            >Informasi Pegawai</span
 	          >
 	          <div class="grid gap-2 md:w-[80%]">
 	            <div class="flex flex-col text-[12px] gap-1">
@@ -15,7 +15,7 @@
 	                class="rounded-lg w-[100%] text-[14px] md:text-[16px] h-[3vmax] px-3 border border-[#D9D9D9]"
 	                type="text"
 	                required
-	                placeholder="Masukkan Nama Ruangan"
+	                placeholder="Masukkan Nama Pegawai"
 	                v-model="formData.name"
 	              />
 	            </div>
@@ -41,7 +41,7 @@
 	              />
 	            </div>
 	          <div>
-	            <label for="">Foto Ruangan</label>
+	            <label for="">Foto Pegawai</label>
 	            <label
 	              for="imageinput"
 	              class="flex flex-col items-center justify-center border-2 border-dashed md:rounded-[24px] border-[#131A29] rounded-md cursor-pointer hover:bg-slate-100 opacity-60 p-5 h-[400px]"
